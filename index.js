00788cee470a899b09cecc958f26cac6398a2dca
Elly <ellyjames1999@gmail.com>
2024-12-06 16:29:19 +0300

    finished all

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const rem= document.getElementById('main');
+rem.remove();
+const newHeader= document.createElement('h1');
+newHeader.id= 'victory'
+document.body.appendChild(newHeader);
+const element= document.getElementById('victory')
+element.textContent= "Sir Elly James is the champion"
